67c635a5074d47b4b91710dbd368c6657078f7d7
Maria Theodorou <mtheodorou@signalgenerix.com>
2021-10-15 14:20:36 +0300

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using BruTile.MbTiles;
+using Mapsui;
+using Mapsui.Layers;
+using Mapsui.Projection;
+using SQLite;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadMap();
+            /*
+            Mapsui.Geometries.Point point = new Mapsui.Geometries.Point(33.047564, 34.698850);
+            var sphericalPoint = SphericalMercator.FromLonLat(point.X, point.Y);
+            MyMapControl.Map.Home = n => n.NavigateTo(point, 12, 0, null);*/
+
+            Mapsui.Geometries.Point center = new Mapsui.Geometries.Point(33.047564, 34.698850);
+            Mapsui.Geometries.Point sphericalMercatorCoordinate = SphericalMercator.FromLonLat(center.X, center.Y);
+            MyMapControl.Map.Home = n => n.NavigateTo(sphericalMercatorCoordinate, 12);
+
+            MyMapControl.Map.Widgets.Add(new Mapsui.Widgets.Zoom.ZoomInOutWidget { MarginX = 10, MarginY = 40 });
+
+        }
+
+        private void LoadMap()
+        {
+            try
+            {
+                var mainDir = "../../../";                
+                string path1 = mainDir + @"tiles";
+
+                string path2 = Path.GetFullPath(Path.Combine(path1, "cymap.mbtiles"));
+                var mbTilesTileSource = new MbTilesTileSource(new SQLiteConnectionString(path2, true));
+                var mbTilesLayer = new TileLayer(mbTilesTileSource) { Name = "regular" };
+                MyMapControl.Map.Layers.Add(mbTilesLayer);
+                // MyMapControl.Map.Layers.Add(OpenStreetMap.CreateTileLayer());               
+            } catch (Exception ex)
+            {
+                string message = ex.Message;
+            }
+        }      
+
+     
+    }
+}
---
+++ VectorMbTilesProvider.cs
@@ -0,0 +1,72 @@
+﻿using BruTile;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+using VectorTileRenderer;
+
+namespace MapSuiLib
+{
+    class VectorMbTilesProvider : ITileProvider
+    {
+
+        Style style;
+        VectorTileRenderer.Sources.MbTilesSource provider;
+        string cachePath;
+
+        public VectorMbTilesProvider(string path, string stylePath, string cachePath)
+        {
+            this.cachePath = cachePath;
+            style = new Style(stylePath)
+            {
+                FontDirectory = @"styles/fonts/"
+            };
+
+            provider = new VectorTileRenderer.Sources.MbTilesSource(path);
+            style.SetSourceProvider("openmaptiles", provider);
+
+        }
+
+        public byte[] GetTile(TileInfo tileInfo)
+        {
+            //var newY = (int)Math.Pow(2, zoom) - pos.Y - 1;
+
+            var canvas = new SkiaCanvas();
+            System.Windows.Media.Imaging.BitmapSource bitmapSource;
+
+            try
+            {
+                bitmapSource = Renderer.RenderCached(cachePath, style, canvas, (int)tileInfo.Index.Col, (int)tileInfo.Index.Row, Convert.ToInt32(tileInfo.Index.Level), 256, 256, 1).Result;
+            }
+            catch
+            {
+                return null;
+            }
+
+            return GetBytesFromBitmapSource(bitmapSource);
+        }
+
+        static byte[] GetBytesFromBitmapSource(BitmapSource bmp)
+        {
+            if (bmp == null)
+            {
+                return null;
+            }
+
+            var encoder = new PngBitmapEncoder();
+            //encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
+            // byte[] bit = new byte[0];
+            using (MemoryStream stream = new MemoryStream())
+            {
+                encoder.Frames.Add(BitmapFrame.Create(bmp));
+                encoder.Save(stream);
+                byte[] bit = stream.ToArray();
+                stream.Close();
+
+                return bit;
+            }
+        }
+
+    }
+}
